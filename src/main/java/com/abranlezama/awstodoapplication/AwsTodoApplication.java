fca9ec93572ae8efe54f53872ff53fad551d5829
Abran Lezama <lezama.abran@gmail.com>
2023-04-27 19:32:52 -0700

    Created Spring Boot Todo application project

---
+++ src/main/java/com/abranlezama/awstodoapplication/AwsTodoApplication.java
@@ -0,0 +1,13 @@
+package com.abranlezama.awstodoapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsTodoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AwsTodoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/abranlezama/awstodoapplication/AwsTodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abranlezama.awstodoapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsTodoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
