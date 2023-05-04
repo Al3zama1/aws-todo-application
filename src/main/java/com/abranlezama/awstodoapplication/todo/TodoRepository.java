b61dd0e368d97d5d1fb91b3950a2cc9258a6c180
Abran Lezama <lezama.abran@gmail.com>
2023-05-03 22:29:03 -0700

    Updated todo repository methods

--- src/main/java/com/abranlezama/awstodoapplication/todo/TodoRepository.java
+++ src/main/java/com/abranlezama/awstodoapplication/todo/TodoRepository.java
@@ -5,13 +5,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface TodoRepository extends JpaRepository<Todo, Long> {
 
-    List<Todo> findAllByOwner(Person person);
-
-    List<Todo> findAllByOwnerEmail(String email);
-
     List<Todo> findAllByOwnerEmailOrderByIdAsc(String email);
+
+    List<Todo> findAllByCollaboratorsEmailOrderByIdAsc(String email);
+
+    Optional<Todo> findByIdAndOwnerEmail(Long todoId, String todoOwnerEmail);
 }
