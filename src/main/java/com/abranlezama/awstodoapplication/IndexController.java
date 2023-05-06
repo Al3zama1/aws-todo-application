e6c0df754146175c4bef3f682a42066d887ed843
Abran Lezama <lezama.abran@gmail.com>
2023-05-05 20:10:14 -0700

    Added event to track user when base page is reached.
    - Removed devtools dependency that was causing issues with
      DynamoDbTemplate.

--- src/main/java/com/abranlezama/awstodoapplication/IndexController.java
+++ src/main/java/com/abranlezama/awstodoapplication/IndexController.java
@@ -1,13 +1,27 @@
 package com.abranlezama.awstodoapplication;
 
+import com.abranlezama.awstodoapplication.tracing.TracingEvent;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.security.Principal;
 
 @Controller
+@RequestMapping("/")
+@RequiredArgsConstructor
 public class IndexController {
 
+    private final ApplicationEventPublisher eventPublisher;
+
     @GetMapping
-    public String getIndex() {
+    public String getIndex(Principal principal) {
+        this.eventPublisher.publishEvent(
+                new TracingEvent(this, "index", principal != null ? principal.getName() : "anonymous")
+        );
+
         return "index";
     }
 }
